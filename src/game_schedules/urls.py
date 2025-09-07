bb957f1dd5d6ad49f649aecf8f8b079336b5603f
MAtterholt <matterholt.dev@gmail.com>
2025-09-07 18:49:24 -0400

    small entry for work

--- src/game_schedules/urls.py
+++ src/game_schedules/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views.base import ScheduledGames
-from .views.venues import VenueListView, VenueCreateView
+from .views.venues import VenueListView, VenueCreateView,VenueCreatePage
 from .views.api import VenueDeleteHTMX
 from .views.base import spreadsheet_upload
 # , VenueUpdateView, VenueDeleteView
@@ -11,7 +11,7 @@ urlpatterns = [
     path("locations", VenueListView.as_view(), name="locations_list"),
 
     path("locations/spreadsheet_upload/", spreadsheet_upload, name="spreadsheet_upload"),
-    path("api/locations/create", VenueCreateView.as_view(), name="create_location"),
+    path("api/locations/create", VenueCreatePage.as_view(), name="create_location"),
     path("api/locations/<int:pk>/details", VenueDeleteHTMX.as_view(), name="details_location_entry"),
     path("api/locations/<int:pk>/delete", VenueDeleteHTMX.as_view(), name="delete_location_entry"),
     path("api/locations/<int:pk>/edit", VenueDeleteHTMX.as_view(), name="edit_location_entry"),
--- src/game_schedules/views/api.py
+++ src/game_schedules/views/api.py
@@ -10,16 +10,6 @@ class VenueDeleteHTMX(DeleteView):
   model = Venue
   success_url = reverse_lazy('locations_list')
 
-  def delete(self, request, *args, **kwargs) -> HttpResponse | HttpResponseRedirect:
-
-    self.object = self.get_object()
-    self.object.delete()
-
-    if request.headers.get('HX-Request'):
-      return HttpResponse(status=204)
-
-    # return super().delete(request, *args, **kwargs)
-    return HttpResponseRedirect(self.success_url)
 
 
 class VenueCreateView(CreateView):
