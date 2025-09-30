f8f46a25c928acc263c9dd29cd0030921e955950
MAtterholt <matterholt.dev@gmail.com>
2025-09-30 07:50:21 -0400

    filter locaiton and css cleanup

--- src/game_schedules/static/script.js
+++ src/game_schedules/static/script.js
@@ -1,12 +1,41 @@
-const dialog = document.querySelector("dialog");
-const showButton = document.querySelector("#newLocation");
-const closeButton = document.querySelector("dialog button");
+// dialog not implemented but holding on to make it work
+//
+// const dialog = document.querySelector("dialog");
+// const showButton = document.querySelector("#newLocation");
+// const closeButton = document.querySelector("dialog button");
 
-showButton.addEventListener("click", () => {
-  dialog.showModal();
+// showButton.addEventListener("click", () => {
+//   dialog.showModal();
+// });
+
+// // "Close" button closes the dialog
+// closeButton.addEventListener("click", () => {
+//   dialog.close();
+// });
+
+const searchBox = document.getElementById("searchBox");
+const locationList = document.getElementById("venue-list");
+const locations = locationList.getElementsByTagName("li");
+
+searchBox.addEventListener("keyup", () => {
+  const filterKey = searchBox.value.toLowerCase();
+
+  for (let i = 0; i < locations.length; i++) {
+    const text = locations[i].textContent.toLowerCase().trim().split(/\s+/);
+    const hasSomeMatch = text.some((x) => x.includes(filterKey));
+
+    if (hasSomeMatch) {
+      locations[i].style.display = "";
+    } else {
+      locations[i].style.display = "none";
+    }
+  }
 });
 
-// "Close" button closes the dialog
-closeButton.addEventListener("click", () => {
-  dialog.close();
+const resetButton = document.getElementById(
+  "storedLocation_searchAction-reset",
+);
+resetButton.addEventListener("click", () => {
+  searchBox.value = "";
+  searchBox.dispatchEvent(new Event("keyup"));
 });
